b62bfc583eb398624a244c58c0e1758148130719
pdbarnesCSUF <pdbarnes@csu.fullerton.edu>
2018-10-30 11:00:02 -0700

    done 5

--- 5_HomeSales/HomeSales.cs
+++ 5_HomeSales/HomeSales.cs
@@ -28,8 +28,60 @@ namespace _5_HomeSales
         {
             WriteLine("hw4#5 - Patrick Barnes");
             WriteLine("HomeSales");
+            int dCount = 0;
+            int eCount = 0;
+            int fCount = 0;
+            double dSum = 0;
+            double eSum = 0;
+            double fSum = 0;
+            
+            char in_who = 'n';
 
+            WriteLine("---Enter Z to stop---");
 
+            while (in_who != 'Z')
+            {
+                Write("Who? (D,E,F) :");
+                in_who = char.ToUpper(ReadLine()[0]);
+
+                switch (in_who)
+                {
+                    case 'D':
+                        Write("(Danielle) Enter Sale:");
+                        dSum += double.Parse(ReadLine());
+                        dCount++;
+                        break;
+                    case 'E':
+                        Write("(Edward) Enter Sale:");
+                        eSum += double.Parse(ReadLine());
+                        eCount++;
+                        break;
+                    case 'F':
+                        Write("(Francis) Enter Sale:");
+                        fSum += double.Parse(ReadLine());
+                        fCount++;
+                        break;
+                    case 'Z':
+                        //quit
+                        break;
+                    default:
+                        WriteLine("Error");
+                        break;
+                }
+            }
+            WriteLine("Danielle Total:{0}", dSum);
+            WriteLine("Edward   Total:{0}", eSum);
+            WriteLine("Francis  Total:{0}", fSum);
+            WriteLine("Grand    Total:{0}", dSum+eSum+fSum);
+            if (dSum > eSum)
+            {
+                if (dSum > fSum)
+                    WriteLine("Danielle sold the most!");
+                else
+                    WriteLine("Francis sold the most!");
+            }
+            else if (eSum > fSum)
+                WriteLine("Edward sold the most");
 
             WriteLine("--End--");
         }

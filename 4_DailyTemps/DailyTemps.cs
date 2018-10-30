9271af372f42f5d33ff271057f29b4ebebe1574f
pdbarnesCSUF <pdbarnes@csu.fullerton.edu>
2018-10-30 10:30:32 -0700

    done 4

--- 4_DailyTemps/DailyTemps.cs
+++ 4_DailyTemps/DailyTemps.cs
@@ -26,8 +26,27 @@ namespace _4_DailyTemps
         {
             WriteLine("hw4#4 - Patrick Barnes");
             WriteLine("DailyTemps");
+            int count = 0;
+            double sum = 0;
+            double entry = 0;
 
-
+            WriteLine("Enter 999 to stop");
+            while (entry != 999)
+            {
+                Write("Temp {0}:", count);
+                entry = double.Parse(ReadLine());
+                if (entry >= -20 && entry <= 130)
+                {
+                    count++;
+                    sum += entry;
+                }
+                else if (entry != 999)
+                {
+                    WriteLine("Error, -20 to 130 . 999 to quit.");
+                }
+            }
+            WriteLine("Count:{0}", count);
+            WriteLine("Avg  :{0}", sum / count);
 
             WriteLine("--End--");
         }
